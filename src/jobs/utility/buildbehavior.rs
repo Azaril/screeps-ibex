ea6f6035ea5cf1f373c17c572575f91996e4b51e
William Archbell <william@archbell.com>
2020-03-01 20:35:54 -0800

    Add additional haul targets.

--- src/jobs/utility/buildbehavior.rs
+++ src/jobs/utility/buildbehavior.rs
@@ -3,37 +3,6 @@ use crate::remoteobjectid::*;
 use crate::room::data::*;
 use screeps::*;
 
-pub struct BuildBehaviorUtility;
-
-impl BuildBehaviorUtility {
-    pub fn build_construction_site(creep: &Creep, construction_site: &ConstructionSite) {
-        scope_timing!("build_construction_site");
-
-        let creep_pos = creep.pos();
-        let target_pos = construction_site.pos();
-
-        if creep_pos.in_range_to(&target_pos, 3) && creep_pos.room_name() == target_pos.room_name() {
-            creep.build(&construction_site);
-        } else {
-            creep.move_to(&target_pos);
-        }
-    }
-
-    pub fn build_construction_site_id(creep: &Creep, construction_site_id: &RemoteObjectId<ConstructionSite>) {
-        let target_position = construction_site_id.pos();
-
-        if creep.pos().room_name() != target_position.room_name() {
-            creep.move_to(&target_position);
-        } else if let Some(construction_site) = construction_site_id.resolve() {
-            //TODO: Handle error code.
-            Self::build_construction_site(creep, &construction_site)
-        } else {
-            //TODO: Return error result.
-            error!("Failed to resolve controller for upgrading. Name: {}", creep.name());
-        }
-    }
-}
-
 pub fn get_new_build_state<F, R>(creep: &Creep, build_room: &RoomData, state_map: F) -> Option<R>
 where
     F: Fn(RemoteObjectId<ConstructionSite>) -> R,
--- src/jobs/utility/controllerbehavior.rs
+++ src/jobs/utility/controllerbehavior.rs
@@ -2,34 +2,6 @@ use crate::remoteobjectid::*;
 use crate::room::data::*;
 use screeps::*;
 
-pub struct ControllerBehaviorUtility;
-
-impl ControllerBehaviorUtility {
-    pub fn upgrade_controller(creep: &Creep, controller: &StructureController) {
-        scope_timing!("upgrade_controller");
-
-        if creep.pos().is_near_to(controller) {
-            creep.upgrade_controller(controller);
-        } else {
-            creep.move_to(controller);
-        }
-    }
-
-    pub fn upgrade_controller_id(creep: &Creep, controller_id: &RemoteObjectId<StructureController>) {
-        let target_position = controller_id.pos();
-
-        if creep.pos().room_name() != target_position.room_name() {
-            creep.move_to(&target_position);
-        } else if let Some(controller) = controller_id.resolve() {
-            //TODO: Handle error code.
-            Self::upgrade_controller(creep, &controller)
-        } else {
-            //TODO: Return error result.
-            error!("Failed to resolve controller for upgrading. Name: {}", creep.name());
-        }
-    }
-}
-
 pub fn get_new_upgrade_state<F, R>(creep: &Creep, upgrade_room: &RoomData, state_map: F) -> Option<R>
 where
     F: Fn(RemoteObjectId<StructureController>) -> R,
