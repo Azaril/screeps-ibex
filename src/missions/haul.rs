227b761308004f3754f7b9e0f46d282154abfcce
William Archbell <william@archbell.com>
2020-04-09 09:48:12 -0700

    Use existing claim code to build spawns. Add hauler if links are full.

--- src/missions/haul.rs
+++ src/missions/haul.rs
@@ -106,9 +106,32 @@ impl Mission for HaulMission {
 
         let base_amount = controller.level() * 500;
 
-        let desired_haulers = (total_unfufilled / base_amount).min(3);
+        let mut desired_haulers = (total_unfufilled as f32 / base_amount as f32).ceil().min(3.0) as usize;
 
-        let should_spawn = self.haulers.0.len() < desired_haulers as usize;
+        if let Some(storage) = room.storage() {
+            let storage_pos = storage.pos();
+            let structures = room.find(find::STRUCTURES);
+
+            let room_links: Vec<_> = structures
+                .iter()
+                .filter_map(|structure| match structure {
+                    Structure::Link(link) => Some(link),
+                    _ => None,
+                })
+                .collect();
+
+            let storage_link_full = room_links
+                .into_iter()
+                .filter(|link| link.pos().in_range_to(&storage_pos, 2))
+                .map(|link| link.store_used_capacity(Some(ResourceType::Energy)) as f32 / link.store_capacity(Some(ResourceType::Energy)) as f32)
+                .any(|store_ratio| store_ratio > 0.9);
+
+            if storage_link_full {
+                desired_haulers += 1;
+            }
+        }
+
+        let should_spawn = self.haulers.0.len() < desired_haulers;
 
         if should_spawn {
             let energy_to_use = if self.haulers.0.is_empty() {
