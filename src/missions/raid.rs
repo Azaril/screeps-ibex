c629b27524ddab35542843ebb67c2e7508752a5c
William Archbell <william@archbell.com>
2020-04-26 23:11:02 -0700

    Fix backwards haul for raiding.

--- src/missions/raid.rs
+++ src/missions/raid.rs
@@ -50,7 +50,7 @@ impl RaidMission {
             let name = name.to_string();
 
             spawn_system_data.updater.exec_mut(move |world| {
-                let creep_job = JobData::Haul(HaulJob::new( &[delivery_room], &[raid_room]));
+                let creep_job = JobData::Haul(HaulJob::new( &[raid_room], &[delivery_room]));
 
                 let creep_entity = crate::creep::spawning::build(world.create_entity(), &name).with(creep_job).build();
 
