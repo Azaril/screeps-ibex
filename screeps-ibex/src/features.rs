6eb5fa6fe4110bf47b3b0dfe5214ebb6ffb84b34
William Archbell <william@archbell.com>
2020-06-13 07:28:31 -0700

    Default new pathfinding to on.

--- screeps-ibex/src/features.rs
+++ screeps-ibex/src/features.rs
@@ -43,7 +43,7 @@ pub mod js {
                     raid: true,
                     dismantle: true,
                     pathing: {
-                        custom: false
+                        custom: true
                     }
                 }
             });
