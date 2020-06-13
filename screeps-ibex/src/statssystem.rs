332f12927905dbc2ee5d5752f9c836727e39f5c6
William Archbell <william@archbell.com>
2020-06-13 15:03:04 -0700

    Detailed storage stats

--- screeps-ibex/src/statssystem.rs
+++ screeps-ibex/src/statssystem.rs
@@ -2,6 +2,7 @@ use super::memorysystem::*;
 use crate::room::data::*;
 use screeps::*;
 use serde::*;
+use serde::ser::SerializeMap;
 use specs::prelude::*;
 use std::collections::HashMap;
 
@@ -12,13 +13,173 @@ pub struct CpuStats {
     used: f64,
 }
 
+fn to_structure_name(val: StructureType) -> &'static str {
+    match val {
+        StructureType::Spawn => "spawn",
+        StructureType::Extension => "extension",
+        StructureType::Road => "road",
+        StructureType::Wall => "wall",
+        StructureType::Rampart => "rampart",
+        StructureType::KeeperLair => "keeperLair",
+        StructureType::Portal => "portal",
+        StructureType::Controller => "controller",
+        StructureType::Link => "link",
+        StructureType::Storage => "storage",
+        StructureType::Tower => "tower",
+        StructureType::Observer => "observer",
+        StructureType::PowerBank => "powerBank",
+        StructureType::PowerSpawn => "powerSpawn",
+        StructureType::Extractor => "extractor",
+        StructureType::Lab => "lab",
+        StructureType::Terminal => "terminal",
+        StructureType::Container => "container",
+        StructureType::Nuker => "nuker",
+        StructureType::Factory => "factory",
+        StructureType::InvaderCore => "invaderCore",
+    }
+}
+
+fn to_resource_name(val: ResourceType) -> &'static str {
+    match val {
+        ResourceType::Energy => "energy",
+        ResourceType::Power => "power",
+        ResourceType::Hydrogen => "H",
+        ResourceType::Oxygen => "O",
+        ResourceType::Utrium => "U",
+        ResourceType::Lemergium => "L",
+        ResourceType::Keanium => "K",
+        ResourceType::Zynthium => "Z",
+        ResourceType::Catalyst => "X",
+        ResourceType::Ghodium => "G",
+        ResourceType::Hydroxide => "OH",
+        ResourceType::ZynthiumKeanite => "ZK",
+        ResourceType::UtriumLemergite => "UL",
+        ResourceType::UtriumHydride => "UH",
+        ResourceType::UtriumOxide => "UO",
+        ResourceType::KeaniumHydride => "KH",
+        ResourceType::KeaniumOxide => "KO",
+        ResourceType::LemergiumHydride => "LH",
+        ResourceType::LemergiumOxide => "LO",
+        ResourceType::ZynthiumHydride => "ZH",
+        ResourceType::ZynthiumOxide => "ZO",
+        ResourceType::GhodiumHydride => "GH",
+        ResourceType::GhodiumOxide => "GO",
+        ResourceType::UtriumAcid => "UH2O",
+        ResourceType::UtriumAlkalide => "UHO2",
+        ResourceType::KeaniumAcid => "KH2O",
+        ResourceType::KeaniumAlkalide => "KHO2",
+        ResourceType::LemergiumAcid => "LH2O",
+        ResourceType::LemergiumAlkalide => "LHO2",
+        ResourceType::ZynthiumAcid => "ZH2O",
+        ResourceType::ZynthiumAlkalide => "ZHO2",
+        ResourceType::GhodiumAcid => "GH2O",
+        ResourceType::GhodiumAlkalide => "GHO2",
+        ResourceType::CatalyzedUtriumAcid => "XUH2O",
+        ResourceType::CatalyzedUtriumAlkalide => "XUHO2",
+        ResourceType::CatalyzedKeaniumAcid => "XKH2O",
+        ResourceType::CatalyzedKeaniumAlkalide => "XKHO2",
+        ResourceType::CatalyzedLemergiumAcid => "XLH2O",
+        ResourceType::CatalyzedLemergiumAlkalide => "XLHO2",
+        ResourceType::CatalyzedZynthiumAcid => "XZH2O",
+        ResourceType::CatalyzedZynthiumAlkalide => "XZHO2",
+        ResourceType::CatalyzedGhodiumAcid => "XGH2O",
+        ResourceType::CatalyzedGhodiumAlkalide => "XGHO2",
+        ResourceType::Ops => "ops",
+        ResourceType::Silicon => "silicon",
+        ResourceType::Metal => "metal",
+        ResourceType::Biomass => "biomass",
+        ResourceType::Mist => "mist",
+        ResourceType::UtriumBar => "utrium_bar",
+        ResourceType::LemergiumBar => "lemergium_bar",
+        ResourceType::ZynthiumBar => "zynthium_bar",
+        ResourceType::KeaniumBar => "keanium_bar",
+        ResourceType::GhodiumMelt => "ghodium_melt",
+        ResourceType::Oxidant => "oxidant",
+        ResourceType::Reductant => "reductant",
+        ResourceType::Purifier => "purifier",
+        ResourceType::Battery => "battery",
+        ResourceType::Composite => "composite",
+        ResourceType::Crystal => "crystal",
+        ResourceType::Liquid => "liquid",
+        ResourceType::Wire => "wire",
+        ResourceType::Switch => "switch",
+        ResourceType::Transistor => "transistor",
+        ResourceType::Microchip => "microchip",
+        ResourceType::Circuit => "circuit",
+        ResourceType::Device => "device",
+        ResourceType::Cell => "cell",
+        ResourceType::Phlegm => "phlegm",
+        ResourceType::Tissue => "tissue",
+        ResourceType::Muscle => "muscle",
+        ResourceType::Organoid => "organoid",
+        ResourceType::Organism => "organism",
+        ResourceType::Alloy => "alloy",
+        ResourceType::Tube => "tube",
+        ResourceType::Fixtures => "fixtures",
+        ResourceType::Frame => "frame",
+        ResourceType::Hydraulics => "hydraulics",
+        ResourceType::Machine => "machine",
+        ResourceType::Condensate => "condensate",
+        ResourceType::Concentrate => "concentrate",
+        ResourceType::Extract => "extract",
+        ResourceType::Spirit => "spirit",
+        ResourceType::Emanation => "emanation",
+        ResourceType::Essence => "essence",
+    }
+}
+
+use shrinkwraprs::*;
+
+#[derive(Shrinkwrap)]
+#[derive(Default)]
+#[shrinkwrap(mutable)]
+struct StorageResource(HashMap<ResourceType, u32>);
+
+impl Serialize for StorageResource {
+    fn serialize<S>(&self, s: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut m = s.serialize_map(Some(self.0.len()))?;
+    
+        for (k, v) in &self.0 {
+            let k = to_resource_name(*k);
+
+            m.serialize_entry(k, v)?;
+        }
+
+        m.end()
+    }
+}
+
+#[derive(Shrinkwrap)]
+#[derive(Default)]
+#[shrinkwrap(mutable)]
+struct StorageStructure(HashMap<StructureType, StorageResource>);
+
+impl Serialize for StorageStructure {
+    fn serialize<S>(&self, s: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut m = s.serialize_map(Some(self.0.len()))?;
+    
+        for (k, v) in &self.0 {
+            let k = to_structure_name(*k);
+
+            m.serialize_entry(k, v)?;
+        }
+
+        m.end()
+    }
+}
+
 #[derive(Serialize)]
 pub struct RoomStats {
     energy_available: u32,
     energy_capacity_available: u32,
 
-    storage_energy: u32,
-    terminal_energy: u32,
+    storage: StorageStructure,
 
     controller_progress: u32,
     controller_progress_total: u32,
@@ -82,24 +243,37 @@ impl StatsSystem {
             })
             .filter_map(|(_, room_data)| {
                 if let Some(room) = game::rooms::get(room_data.name) {
-                    let controller = room.controller();
+                    let controller = room.controller()?;
+
+                    let structures = room_data.get_structures()?;
+                    let mut storage: StorageStructure = StorageStructure::default();
+
+                    for structure in structures.all().iter() {
+                        let structure_type = structure.structure_type();
+
+                        if let Some(store) = structure.as_has_store() {
+                            let structure_storage = storage.entry(structure_type)
+                                .or_insert_with(StorageResource::default);
+
+                            for resource_type in store.store_types() {
+                                let amount = store.store_of(resource_type);
+
+                                structure_storage.entry(resource_type)
+                                    .and_modify(|e| *e += amount)
+                                    .or_insert(amount);
+                            }
+                        }
+                    }
 
                     let stats = RoomStats {
                         energy_available: room.energy_available(),
                         energy_capacity_available: room.energy_capacity_available(),
 
-                        storage_energy: room
-                            .storage()
-                            .map(|s| s.store_used_capacity(Some(ResourceType::Energy)))
-                            .unwrap_or(0),
-                        terminal_energy: room
-                            .terminal()
-                            .map(|s| s.store_used_capacity(Some(ResourceType::Energy)))
-                            .unwrap_or(0),
+                        storage,
 
-                        controller_progress: controller.as_ref().and_then(|c| c.progress()).unwrap_or(0),
-                        controller_progress_total: controller.as_ref().and_then(|c| c.progress_total()).unwrap_or(0),
-                        controller_level: controller.as_ref().map(|c| c.level()).unwrap_or(0),
+                        controller_progress: controller.progress().unwrap_or(0),
+                        controller_progress_total: controller.progress_total().unwrap_or(0),
+                        controller_level: controller.level(),
                     };
 
                     Some((room_data.name, stats))
