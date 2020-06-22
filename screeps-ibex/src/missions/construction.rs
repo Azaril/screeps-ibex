68eab1ebad35e32daae6d76f0c522c2c88354a9f
William Archbell <wiarchbe@microsoft.com>
2020-06-22 13:15:21 -0700

    Fix claim starve out due to construction site saturation.

--- screeps-ibex/src/missions/construction.rs
+++ screeps-ibex/src/missions/construction.rs
@@ -61,7 +61,16 @@ impl Mission for ConstructionMission {
         let request_plan = if let Some(room_plan_data) = system_data.room_plan_data.get(self.room_data) {
             if let Some(plan) = room_plan_data.plan() {
                 if (game::time() % 50 == 0) && crate::features::construction::execute() {
-                    plan.execute(&room);
+
+                    let construction_sites = room_data.get_construction_sites().ok_or("Expected construction sites")?;
+
+                    const MAX_CONSTRUCTION_SITES: i32 = 10;
+
+                    let max_placement = MAX_CONSTRUCTION_SITES - (construction_sites.len() as i32);
+
+                    if max_placement > 0 {
+                        plan.execute(&room, max_placement as u32);
+                    }
                 }
 
                 false
