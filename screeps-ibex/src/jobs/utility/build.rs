999a2486b62cebb55b7b49731dc78c3ba0177a61
William Archbell <william@archbell.com>
2020-06-28 19:18:34 -0700

    Simplify construction site selection.

--- screeps-ibex/src/jobs/utility/build.rs
+++ screeps-ibex/src/jobs/utility/build.rs
@@ -3,20 +3,17 @@ use screeps_foreman::planner::*;
 
 #[cfg_attr(feature = "profile", screeps_timing_annotate::timing)]
 pub fn select_construction_site(creep: &Creep, construction_sites: &[ConstructionSite]) -> Option<ConstructionSite> {
-    let mut priority_construction_sites: Vec<_> = construction_sites
+    let creep_pos = creep.pos();
+
+    construction_sites
         .iter()
         .filter(|s| s.my())
         .map(|s| (s, get_build_priority(s.structure_type())))
-        .collect();
-
-    let creep_pos = creep.pos();
-
-    priority_construction_sites.sort_by(|(a, a_priority), (b, b_priority)| {
-        a_priority
-            .cmp(b_priority)
-            .then_with(|| a.progress().cmp(&b.progress()))
-            .then_with(|| creep_pos.get_range_to(&a.pos()).cmp(&creep_pos.get_range_to(&b.pos())).reverse())
-    });
-
-    priority_construction_sites.pop().take().map(|(s, _)| s.clone())
+        .max_by(|(a, a_priority), (b, b_priority)| {
+            a_priority
+                .cmp(b_priority)
+                .then_with(|| a.progress().cmp(&b.progress()))
+                .then_with(|| creep_pos.get_range_to(&a.pos()).cmp(&creep_pos.get_range_to(&b.pos())).reverse())
+        })
+        .map(|(s, _)| s.clone())
 }
